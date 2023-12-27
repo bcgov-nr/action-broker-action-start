f8ef65558bc2e979cada82742720f497246593e7
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2023-12-27 15:25:19 -0800

    fix: error checking

--- action-start.sh
+++ action-start.sh
@@ -4,9 +4,10 @@ echo "===> Action start"
 
 # Use saved action token to start it
 RESPONSE=$(curl -s -X POST $BROKER_URL/v1/intention/action/start -H 'X-Broker-Token: '"$ACTION_TOKEN"'')
+code=$?
 
-if [ "$(echo $RESPONSE | jq '.error')" != "null" ]; then
-    echo "Exit: Error detected"
+if [ "$code" != "0" ]; then
+    echo "Exit: Error detected ($code)"
     echo $RESPONSE | jq '.'
     exit 1
 fi
